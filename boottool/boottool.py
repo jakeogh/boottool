74e8a48e02816cd1121d558f28414ab17b337e8d
jakeogh <github.com@v6y.net>
2021-10-05 21:06:38 -0700

    auto-commit

--- boottool/boottool.py
+++ boottool/boottool.py
@@ -25,19 +25,42 @@
 import os
 import sys
 import time
+from signal import SIG_DFL
+from signal import SIGPIPE
+from signal import signal
+
+import click
+import sh
+
+signal(SIGPIPE, SIG_DFL)
+import os
+import sys
+import time
 from pathlib import Path
 from signal import SIG_DFL
 from signal import SIGPIPE
 from signal import signal
+from typing import ByteString
+from typing import Generator
+from typing import Iterable
 from typing import Iterator
+from typing import List
+from typing import Optional
+from typing import Sequence
 from typing import Tuple
+from typing import Union
 
 import click
 import humanfriendly
 import sh
+#from with_sshfs import sshfs
+#from with_chdir import chdir
 from asserttool import eprint
 from asserttool import ic
+from asserttool import nevd
 from asserttool import root_user
+from asserttool import validate_slice
+from asserttool import verify
 from blocktool import add_partition_number_to_device
 from blocktool import create_filesystem
 from blocktool import destroy_block_device_head_and_tail
@@ -53,9 +76,16 @@ from devicetool import path_is_block_special
 from devicetool import write_efi_partition
 from devicetool import write_gpt
 from devicetool import write_grub_bios_partition
+from enumerate_input import enumerate_input
 from mounttool import block_special_path_is_mounted
 from mounttool import path_is_mounted
+#from asserttool import not_root
+#from pathtool import path_is_block_special
+from pathtool import write_line_to_file
+from portagetool import install_package
 from psutil import virtual_memory
+from retry_on_exception import retry_on_exception
+from timetool import get_timestamp
 
 signal(SIGPIPE, SIG_DFL)
 from pathlib import Path
@@ -321,17 +351,97 @@ def create_boot_device_for_existing_root(ctx,
     sh.grub_mkconfig('-o', '/boot/grub/grub.cfg', _out=sys.stdout, _err=sys.stderr)
 
 
-#@click.command()
-#@click.option('--device', is_flag=False, required=True)
-#@click.option('--partition-table', is_flag=False, required=True, type=click.Choice(['gpt']), default='gpt')
-#@click.option('--filesystem', is_flag=False, required=True, type=click.Choice(['ext4', 'zfs']), default='ext4')
-#@click.option('--force', is_flag=True,  required=False)
-#@click.pass_context
-#def main(ctx, device, partition_table, filesystem, force):
-#    create_boot_device(ctx,
-#                       device=device,
-#                       partition_table=partition_table,
-#                       filesystem=filesystem,
-#                       force=force,
-#                       verbose=ctx['verbose'],
-#                       debug=ctx['debug'],)
+@cli.command()
+@click.argument("boot_device",
+                type=click.Path(exists=True,
+                                dir_okay=False,
+                                file_okay=True,
+                                allow_dash=False,
+                                path_type=Path,),
+                nargs=1,
+                required=True,)
+@click.option('--verbose', is_flag=True)
+@click.option('--debug', is_flag=True)
+@click.pass_context
+def install_grub(ctx,
+                 boot_device: Path,
+                 verbose: bool,
+                 debug: bool,
+                 ):
+
+    null, end, verbose, debug = nevd(ctx=ctx,
+                                     printn=False,
+                                     ipython=False,
+                                     verbose=verbose,
+                                     debug=debug,)
+
+    if not path_is_mounted(Path("/boot/efi"), verbose=verbose, debug=debug,):
+        ic("/boot/efi not mounted. Exiting.")
+        sys.exit(1)
+
+    sh.env_update()
+    #set +u # disable nounset        # line 22 has an unbound variable: user_id /etc/profile.d/java-config-2.sh
+    #source /etc/profile || exit 1
+    #set -o nounset
+
+    install_package('grub')
+
+    #if [[ "${root_filesystem}" == "zfs" ]];
+    #then
+    #    echo "GRUB_PRELOAD_MODULES=\"part_gpt part_msdos zfs\"" >> /etc/default/grub
+    #   #echo "GRUB_CMDLINE_LINUX_DEFAULT=\"boot=zfs root=ZFS=rpool/ROOT\"" >> /etc/default/grub
+    #   #echo "GRUB_CMDLINE_LINUX_DEFAULT=\"boot=zfs\"" >> /etc/default/grub
+    #   #echo "GRUB_DEVICE=\"ZFS=rpool/ROOT/gentoo\"" >> /etc/default/grub
+    #   # echo "GRUB_DEVICE=\"ZFS=${hostname}/ROOT/gentoo\"" >> /etc/default/grub #this was uncommented, disabled to not use hostname
+    #else
+    write_line_to_file(path=Path('/etc/default/grub'),
+                       line='GRUB_PRELOAD_MODULES="part_gpt part_msdos"' + '\n',
+                       unique=True,
+                       verbose=verbose,
+                       debug=debug,)
+
+    root_partition_command = sh.Command('/home/cfg/linux/disk/get_root_device')
+    root_partition = root_partition_command()
+    ic("-------------- root_partition: {root_partition} ---------------------".format(root_partition=root_partition))
+    partition_uuid_command = sh.Command('/home/cfg/linux/hardware/disk/blkid/PARTUUID')
+    partuuid = partition_uuid_command(root_partition)
+    ic("GRUB_DEVICE partuuid: {partuuid}".format(partuuid=partuuid))
+
+    write_line_to_file(path=Path('/etc/default/grub'),
+                       line='GRUB_DEVICE="PARTUUID={partuuid}"'.format(partuuid=partuuid) + '\n',
+                       unique=True,
+                       verbose=verbose,
+                       debug=debug,)
+    partuuid_root_device_command = sh.Command('/home/cfg/linux/disk/blkid/PARTUUID_root_device')
+    partuuid_root_device = partuuid_root_device_command()
+
+
+    partuuid_root_device_fstab_line = 'PARTUUID=' + partuuid_root_device + '\t/' + '\text4' + '\tnoatime' + '\t0'+ '\t1'
+    write_line_to_file(path=Path('/etc/fstab'),
+                       line=partuuid_root_device_fstab_line + '\n',
+                       unique=True,
+                       verbose=verbose,
+                       debug=debug,)
+
+    #grep -E "^GRUB_CMDLINE_LINUX=\"net.ifnames=0 rootflags=noatime irqpoll\"" /etc/default/grub || { echo "GRUB_CMDLINE_LINUX=\"net.ifnames=0 rootflags=noatime irqpoll\"" >> /etc/default/grub ; }
+    write_line_to_file(path=Path('/etc/default/grub'),
+                       line='GRUB_CMDLINE_LINUX="net.ifnames=0 rootflags=noatime intel_iommu=off"' + '\n',
+                       unique=True,
+                       verbose=verbose,
+                       debug=debug,)
+
+    sh.ln('-sf', '/proc/self/mounts', '/etc/mtab')
+
+    sh.grub_install('--compress=no', '--target=x86_64-efi', '--efi-directory=/boot/efi', '--boot-directory=/boot', '--removable', '--recheck', '--no-rs-codes', boot_device, _out=sys.stdout, _err=sys.stderr)
+    sh.grub_install('--compress=no', '--target=i386-pc', '--boot-directory=/boot', '--recheck', '--no-rs-codes', boot_device, _out=sys.stdout, _err=sys.stderr)
+
+    sh.grub_mkconfig('-o', '/boot/grub/grub.cfg', _out=sys.stdout, _err=sys.stderr)
+
+    with open('/install_status', 'a') as fh:
+        fh.write(get_timestamp() + sys.argv[0] + 'complete'  + '\n')
+
+
+if __name__ == '__main__':
+    # pylint: disable=E1120
+    cli()
+
