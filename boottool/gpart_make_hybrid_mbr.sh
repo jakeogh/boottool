940207171b274287d577af7422afd03a89ef6aa9
jakeogh <github.com@v6y.net>
2023-07-08 01:33:09 -0700

    auto-commit

--- boottool/gpart_make_hybrid_mbr.sh
+++ boottool/gpart_make_hybrid_mbr.sh
@@ -9,5 +9,5 @@ test -b "${device}" || { echo "$0: ${device} not found or is not a block device.
 #mount | grep "${device}" && { echo "$0: ${device} is mounted. Exiting." ; exit 1 ; }
 
 echo "$0: making hybrid MBR"
-/home/cfg/_myapps/sendgentoo/sendgentoo/gpart_make_hybrid_mbr.exp "${device}" || exit 1
+./gpart_make_hybrid_mbr.exp "${device}" || exit 1
 echo "$0: making hybrid MBR exited 0, should be good to go."
